2b0bac85fb65d7fcfdda52814b635705f2163f56
NAVEEN KUMAR <98443253+NAVEEN171@users.noreply.github.com>
2025-01-11 15:41:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",()=>{
-  return res.json({msg:"successfully deployed"});
-})
+
 
 //mongoDb connection
 ConnectDB();
