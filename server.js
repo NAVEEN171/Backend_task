b082441008dae5f5a26d2330fdf763e190639b39
NAVEEN171 <naveenkumar171837@gmail.com>
2025-01-11 14:14:13 +0530

    vercel config added

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ require("dotenv").config();
 const axios = require("axios");
 const mongoose = require("mongoose");
 const ConnectDB = require("./utils/DBconnection");
-const cryptocurrencyRoutes = require("./routes/cryptoRoutes"); 
+const cryptocurrencyRoutes = require("./routes/cryptoRoutes");
+const cors=require("cors"); 
 
 
 const app = express();
 
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
 
 //mongoDb connection
 ConnectDB();
